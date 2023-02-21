f7f4c4e60604bf94c2543c0f38922ef7aa6342fa
Lia Stith <lstith@my.wctc.edu>
2023-02-20 22:29:13 -0600

    cleanup

--- week4Assignmnet/Program.cs
+++ week4Assignmnet/Program.cs
@@ -1,4 +1,4 @@
-﻿using NLog.Web;
+﻿using System.Net;
 
 namespace week4Assignmnet;
 
@@ -9,141 +9,107 @@ internal class Program
         string input;
 
         var file = "movies.csv";
-        var IDs = new List<ulong>();
-        var Titles = new List<string>();
-        var Genres = new List<string>();
+        var Lines = new List<string>();
 
-        var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
 
-        logger.Info("Program started");
+        var check = new checker();
+
+
+        if (File.Exists(file))
         {
-            if (!File.Exists(file))
+            var sr = new StreamReader(file);
+            try
             {
-                //  logger.Error("File does not exist: {file}", file);
-            }
-            else
-            {
-                var sr = new StreamReader(file);
-                try
+                sr.ReadLine();
+
+
+                while (!sr.EndOfStream)
                 {
-                    sr.ReadLine();
-
-
-                    while (!sr.EndOfStream)
-                    {
-                        var line = sr.ReadLine();
-
-                        var ind = line.IndexOf('"');
-                        if (ind == -1)
-                        {
-                            var movie = line.Split(',');
-
-                            IDs.Add(ulong.Parse(movie[0]));
-                            Titles.Add(movie[1]);
-                            Genres.Add(movie[2].Replace("|", ", "));
-                        }
-                        else
-                        {
-                            IDs.Add(ulong.Parse(line.Substring(0, ind - 1)));
-
-                            line = line.Substring(ind + 1);
-                            ind = line.IndexOf('"');
-
-                            Titles.Add(line.Substring(0, ind));
-                            line = line.Substring(ind + 2);
-
-                            Genres.Add(line.Replace("|", ", "));
-                        }
-                    }
-
-
-
-
-                }
-
-                catch (Exception ex)
-                {
-                }
-                finally
-                {
-                    sr.Close();
+                    var line = sr.ReadLine();
+                    Lines.Add(check.validate(line));
                 }
             }
 
-            do
+            catch (Exception ex)
             {
-                Console.WriteLine("1) Read data from file.");
-                Console.WriteLine("2) Create file from data.");
-                Console.WriteLine("Enter any other key to exit.");
-
-                // stored user  input 
-                input = Console.ReadLine();
-
-
-                if (input == "1")
-                {
-                    Console.WriteLine("how many records would you like to see ");
-                    var amount = Convert.ToInt32(Console.ReadLine());
-                    var loop = IDs.Count() - amount;
-
-                    for (var i = loop; i < IDs.Count(); i++)
-                    {
-                        Console.WriteLine($"Id: {IDs[i]}");
-                        Console.WriteLine($"Title: {Titles[i]}");
-                        Console.WriteLine($"Genre(s): {Genres[i]}");
-                        Console.WriteLine();
-                    }
-                }
-
-                else if (input == "2")
-                {
-                    StreamWriter sw = new StreamWriter(file, true);
-
-                    // gathering user input
-                    Console.WriteLine("Movie Id");
-                    UInt64 id = Convert.ToUInt64(Console.ReadLine());
-                    while (IDs.Contains(id))
-                    {
-                        Console.WriteLine($" id {id} already exists make another ID");
-                        id = Convert.ToUInt64(Console.ReadLine());
-
-                    }
-
-
-                    Console.WriteLine("Title");
-                    string title = Console.ReadLine();
-                    while (Titles.Contains(title))
-                    {
-                        Console.WriteLine($" title {title} already exists make another ID");
-                        title = Console.ReadLine();
-
-                    }
-
-                    Console.WriteLine("how many genres");
-                    int genre = Convert.ToInt32(Console.ReadLine());
-
-
-                    // a string array with the sise of watchers variable i can add to in my loop 
-                    String[] genres = new String[genre];
-                    // for loop to store the names the user enters followed by a pipe so i don't have to edit the way the file reads 
-                    for (int i = 0; i < genre; i++)
-                    {
-                        Console.WriteLine("whats the genre");
-                        genres[i] = Console.ReadLine();
-                    }
-
-                    sw.WriteLine($"{id},{title},{string.Join('|', genres)}");
-                    IDs.Add(id);
-                    Titles.Add(title);
-                    Genres.Add(string.Join(',', genres));
-                 
-
-                    sw.Close(); // always close and don't loop your close statement
-
-
-
-                }
-            } while (input == "1" || input == "2");
+            }
+            finally
+            {
+                sr.Close();
+            }
         }
+
+        do
+        {
+            Console.WriteLine("1) Read data from file.");
+            Console.WriteLine("2) Create file from data.");
+            Console.WriteLine("Enter any other key to exit.");
+
+            // stored user  input 
+            input = Console.ReadLine();
+
+
+            if (input == "1")
+            {
+                Console.WriteLine("how many records would you like to see ");
+                var amount = Convert.ToInt32(Console.ReadLine());
+                var loop = Lines.Count - amount;
+
+                for (var i = loop; i < Lines.Count(); i++)
+                {
+                    Console.WriteLine(Lines[i]);
+                    Console.WriteLine();
+                }
+            }
+
+            else if (input == "2")
+            {
+                var sw = new StreamWriter(file, true);
+
+                // gathering user input
+
+              //  var id = Lines.Count + 1;
+
+                
+                 Console.WriteLine("Movie Id");
+                var id = Convert.ToInt64(Console.ReadLine());
+                
+                while (check.IdContains(id))
+                {
+                    Console.WriteLine($"ID {id} already exists make another ID");
+                    id = Convert.ToInt64(Console.ReadLine());
+                }
+
+
+                Console.WriteLine("Title");
+                var title = Console.ReadLine();
+                while (check.TitleContains(title))
+                {
+                    Console.WriteLine($"Title {title} already exists make another Title");
+                    title = Console.ReadLine();
+                }
+
+
+                Console.WriteLine("how many genres");
+                var genre = Convert.ToInt32(Console.ReadLine());
+
+
+                // a string array with the sise of watchers variable i can add to in my loop 
+                var genres = new string[genre];
+                // for loop to store the names the user enters followed by a pipe so i don't have to edit the way the file reads 
+                for (var i = 0; i < genre; i++)
+                {
+                    Console.WriteLine("whats the genre(s)");
+                    genres[i] = Console.ReadLine();
+                }
+
+
+                sw.WriteLine(check.create(id, title, string.Join(',', genres)));
+                Lines.Add(check.create(id, title, string.Join(',', genres)));
+
+
+                sw.Close(); // always close and don't loop your close statement
+            }
+        } while (input == "1" || input == "2");
     }
 }
---
+++ week4Assignmnet/checker.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.VisualBasic;
+
+namespace week4Assignmnet
+{
+    internal class checker
+    {
+       
+        List<Int64> IDs = new List<Int64>();
+        List<string> Titles = new List<string>();
+      
+       
+        private string id;
+        private string title;
+        private string genres;
+
+        public String validate(string line)
+        {
+         
+
+            var ind = line.IndexOf('"');
+            if (ind == -1)
+            {
+                var movie = line.Split(',');
+
+                id = movie[0]; 
+                title = movie[1]; 
+                genres = movie[2].Replace("|", ", ");
+              
+            }
+            else
+            {
+                 id = line.Substring(0, ind - 1);
+
+                line = line.Substring(ind + 1);
+                ind = line.IndexOf('"');
+
+                title = line.Substring(0, ind);
+                line = line.Substring(ind + 2);
+
+                genres = line.Replace("|", ", ");
+
+               
+            }
+            IDs.Add(Int64.Parse(id));
+            Titles.Add(title);
+          
+           
+
+            return $"ID: {id}\nTitle: {title}\nGenre:{genres}";
+        }
+
+
+        public Boolean IdContains(Int64 id)
+        {
+            if (IDs.Contains(id))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public Boolean TitleContains(string title)
+        {
+            if (Titles.Contains(title))
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+
+        public string create(Int64 id,string title, String genres)
+
+        {
+            return $"ID: {id}\nTitle: {title}\nGenre:{genres}";
+
+        }
+    }
+
+  
+}
